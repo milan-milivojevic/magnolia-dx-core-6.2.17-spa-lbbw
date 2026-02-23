5a11dab7c292692a818b4ceb27d2e4b26ebe56f0
Milan Milivojevic <milan.milivojevic@uptempo.io>
2026-02-23 22:40:07 +0100

    Manual cleanup

--- react-spa/standard-template-kit/src/pages/landingPage.js
+++ react-spa/standard-template-kit/src/pages/landingPage.js
@@ -8,8 +8,8 @@ function RedirectPage ({
   const isPagesApp = window.location.search.includes("mgnlPreview");
   const editMode = isPagesApp ? "editMode" : "";
 
-  const href1 = "https://dg.brandmaker.com/cmsPublic";
-  const href2 = "https://dg.brandmaker.com/cmsPublic/Home";
+  const href1 = "https://lbbw.brandmaker.com/cmsPublic";
+  const href2 = "https://lbbw.brandmaker.com/cmsPublic/Home";
   
   window.history.pushState({}, "", href1);
   window.history.pushState({}, "", href2);
